e85b462b366d5d5edc30ca8295a5121a576ecd24
Vero-27 <veronique.bodin@etu.unilim.fr>
2024-03-26 08:23:43 +0100

    Creation du projet

---
+++ VeroniqueApp/VeroniqueApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VeroniqueApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ VeroniqueApp/VeroniqueApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VeroniqueApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
